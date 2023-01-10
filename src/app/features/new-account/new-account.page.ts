6b1bbf46cb913e118334ed838307a548b070f558
GuillaumeDebz <guillaume.debruyne@gmail.com>
2023-01-10 15:54:04 +0100

    visuel créer compte

--- src/app/features/new-account/new-account.page.ts
+++ src/app/features/new-account/new-account.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-account',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewAccountPage implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
   }
+
+    // CREER NOUVEAU COMPTE  //
+    creerCompte(){
+      this.router.navigate(['user']);
+    };
+    
 
 }

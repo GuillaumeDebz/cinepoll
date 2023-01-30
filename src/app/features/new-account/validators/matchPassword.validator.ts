5ec58f3a3cf97581d3eb757513161a837b7df817
GuillaumeDebz <guillaume.debruyne@gmail.com>
2023-01-30 11:23:39 +0100

    confirmpassword

--- src/app/features/new-account/validators/matchPassword.validator.ts
+++ src/app/features/new-account/validators/matchPassword.validator.ts
@@ -4,12 +4,13 @@ export function matchPasswordValidator(controlGroup : AbstractControl){
     
     let error : ValidationErrors | null = null
 
-    if(controlGroup.value["AccountProperties.PASSWORD"] && controlGroup.value["AccountProperties.CONFIRMPASSWORD"]){
+    if(controlGroup.value["password"] && controlGroup.value["confirmpassword"]){
         
-        if(controlGroup.value["AccountProperties.PASSWORD"] != controlGroup.value["AccountProperties.CONFIRMPASSWORD"])
+        if(controlGroup.value["password"] != controlGroup.value["confirmpassword"])
         {
             error = { MatchPassword : "Les mots de passe ne correspondent pas"}            
         }
+        
     }
 
     return error
